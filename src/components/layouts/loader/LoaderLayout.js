90de3cc95770eede95e651814eb11d0d4cec6123
Roman Pansiuk <gfyroman@gmail.com>
2021-08-03 16:28:22 +0300

    sass support

--- src/components/layouts/loader/LoaderLayout.js
+++ src/components/layouts/loader/LoaderLayout.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 
 import {Loader} from '../../loader/Loader';
-import './loader-layout.css';
+import './loader-layout.scss';
 
 const LoaderLayout = ({isLoaderEnabled, children}) => (
   <>
