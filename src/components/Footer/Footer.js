bc5325367e8369f1de37039fd2644bb3a96513fb
Roman Pansiuk <gfyroman@gmail.com>
2021-08-07 13:39:09 +0300

    MATE-24: Main layout was added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="bg-white sticky-footer">
+    <div className="container my-auto">
+      <div className="text-center my-auto copyright">
+        <span>Copyright © Brand 2021</span>
+      </div>
+    </div>
+  </footer>
+);
+
+export default Footer;
--- src/components/layouts/main/MainLayout.js
+++ src/components/layouts/main/MainLayout.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import LeftMenu from '../../LeftMenu/LeftMenu';
+import Header from '../../Header/Header';
+import Footer from '../../Footer/Footer';
 
 export const MainLayout = (props) => (
-  <>
-    <p>main layout</p>
-    {props.children}
-  </>
-)
+  <div id="wrapper">
+    <LeftMenu/>
+    <div className="d-flex flex-column" id="content-wrapper">
+      <div id="content">
+        <Header/>
+        {props.children}
+      </div>
+      <Footer/>
+    </div>
+  </div>
+);
 
 MainLayout.propTypes = {
   children: PropTypes.any,
-}
+};
